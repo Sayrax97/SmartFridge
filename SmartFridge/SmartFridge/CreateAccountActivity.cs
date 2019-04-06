23cdea4603b0e080f11c7a0cd6db4cf42b926518
Xamar <dusann.jankovic@elfak.rs>
2019-04-06 15:30:24 +0200

    ActivityCreate
    Napravio sam main,login i createaAcc activitije i njihove layoute

---
+++ SmartFridge/SmartFridge/CreateAccountActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SmartFridge
+{
+    [Activity(Label = "CreateAccountActivity",MainLauncher = true)]
+    public class CreateAccountActivity : Activity
+    {
+        private EditText usernameEditText;
+        private EditText passwordEditText;
+        private EditText emailEditText;
+        private EditText nameEditText;
+        private EditText surenameEditText;
+        private ImageButton pictureImageButton;
+        private TextView choosePicTextView;
+        private Button okButton;
+        private Button cancelButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.create_account_layout);
+            usernameEditText = FindViewById<EditText>(Resource.Id.editTxtUserNameCreateAcc);
+            passwordEditText = FindViewById<EditText>(Resource.Id.editTxtPasswordCreateAcc);
+            emailEditText = FindViewById<EditText>(Resource.Id.editTxtEmailCreateAcc);
+            nameEditText = FindViewById<EditText>(Resource.Id.editTxtNameCreateAcc);
+            surenameEditText = FindViewById<EditText>(Resource.Id.editTxtSureNameCreateAcc);
+            pictureImageButton = FindViewById<ImageButton>(Resource.Id.imagebtnPictureCreateAcc);
+            okButton = FindViewById<Button>(Resource.Id.btnOkCreateAcc);
+            cancelButton = FindViewById<Button>(Resource.Id.btnCancelCreateAcc);
+            okButton.Click += OkButton_Click;
+            cancelButton.Click += CancelButton_Click;
+
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SmartFridge/SmartFridge/LoginActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SmartFridge
+{
+    [Activity(Label = "LoginActivity")]
+    public class LoginActivity : Activity
+    {
+        private EditText usernameEditText;
+        private EditText passwordEditText;
+        private Button loginButton;
+        private Button createAccountButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.login_layout);
+            usernameEditText = FindViewById<EditText>(Resource.Id.editTxtUsername);
+            passwordEditText = FindViewById<EditText>(Resource.Id.editTxtPassword);
+            loginButton = FindViewById<Button>(Resource.Id.btnLogin);
+            createAccountButton = FindViewById<Button>(Resource.Id.btnCreateAcc);
+            loginButton.Click += LoginButton_Click;
+            createAccountButton.Click += CreateAccountButton_Click;
+        }
+
+        private void CreateAccountButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
